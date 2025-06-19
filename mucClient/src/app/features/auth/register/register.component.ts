bb6fce19e05b53ca04a0af4a696734f69d8af09f
i-am-ballu <49309867+i-am-ballu@users.noreply.github.com>
2025-06-19 20:59:34 +0530

    push

--- mucClient/src/app/features/auth/register/register.component.ts
+++ mucClient/src/app/features/auth/register/register.component.ts
@@ -18,17 +18,17 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.fb.group({
+      company: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
-      company: ['', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      address:['', Validators.required],
+      mobile_number:['', Validators.required],
       country: [null, Validators.required],
       state: [null, Validators.required],
       city: [null, Validators.required],
-      address:['', Validators.required],
-      mobile_number:['', Validators.required],
       agree: [false, Validators.requiredTrue]
     });
     this.getCountries();
@@ -38,7 +38,7 @@ export class RegisterComponent implements OnInit {
   public getCountries(){
     this.loginService.getCountries({}).subscribe({
       next: (res: any) => {
-        this.countrie_list = res && res.countries && res.countries.length ? res.countries : []
+        this.countrie_list = res && res.data && res.data.countries && res.data.countries.length ? res.data.countries : []
         console.log('res -------', res)
       },
       error: err => {
@@ -52,11 +52,11 @@ export class RegisterComponent implements OnInit {
   public onChangeLocation(event){
     this.selected_country = event;
     let obj = {
-      country : this.selected_country,
+      country_code : this.selected_country.iso2,
     }
     this.loginService.getStates(obj).subscribe({
       next: (res: any) => {
-        this.state_list = res && res.states && res.states.length ? res.states : []
+        this.state_list = res && res.data && res.data.states && res.data.states.length ? res.data.states : []
         console.log('res -------', res)
       },
       error: err => {
@@ -68,14 +68,14 @@ export class RegisterComponent implements OnInit {
   public cities_list : any;
   public selected_cities : any;
   public onChangeState(event){
-    this.selected_cities = event.name;
+    this.selected_cities = event;
     let obj = {
-      country : this.selected_country,
-      state : this.selected_cities,
+      country_code : this.selected_country.iso2,
+      state_code : this.selected_cities.iso2,
     }
     this.loginService.getCities(obj).subscribe({
       next: (res: any) => {
-        this.cities_list = res && res.cities && res.cities.length ? res.cities : []
+        this.cities_list = res && res.data && res.data.cities && res.data.cities.length ? res.data.cities : []
         console.log('res -------', res)
       },
       error: err => {
@@ -88,10 +88,33 @@ export class RegisterComponent implements OnInit {
     console.log('event', event)
   }
 
-  onSubmit() {
-    if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
+  public submitted : boolean = false;
+  public onSubmitRegister(){
+    this.submitted = true;
+    if(this.registerForm.invalid){
+      return;
     }
+    let company = this.registerForm.get('company').value;
+    let agree = this.registerForm.get('agree').value;
+    let country = this.registerForm.get('country').value.name;
+    let state = this.registerForm.get('state').value;
+    let city = this.registerForm.get('city').value;
+    let obj = {
+      company: company,
+      company_id : company && company.company_id ? company.company_id : 0,
+      first_name: this.registerForm.get('first_name').value,
+      last_name: this.registerForm.get('last_name').value,
+      email: this.registerForm.get('email').value,
+      password: this.registerForm.get('password').value,
+      confirmPassword: this.registerForm.get('confirmPassword').value,
+      address: this.registerForm.get('address').value,
+      mobile_number: this.registerForm.get('mobile_number').value,
+      country: country,
+      state: state,
+      city: city,
+      agree: agree ? agree : false,
+    }
+
   }
 
 }
